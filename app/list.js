887939c48473027c50a53255c8fe03b482d8ac81
albert-cyberhulk <albert@cyberhulk.net>
2016-04-30 20:43:42 +0300

    adding the files

--- app/list.js
+++ app/list.js
@@ -3,11 +3,18 @@ import Card from './card';
 class List extends Component {
   render() {
     let cards = this.props.cards.map(function (card) {
-      return <Card id={card.id} key={card.id} title={card.title} description={card.description} tasks={card.tasks} />
+      return <Card
+        id={card.id}
+        key={card.id}
+        title={card.title}
+        description={card.description}
+        tasks={card.tasks} />
     });
     return (
       <div className="list">
-        <h1>{this.props.title}</h1>
+        <h1>
+          {this.props.title}
+        </h1>
         {cards}
       </div>
     );
