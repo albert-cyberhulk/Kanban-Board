9662bc1c78bd5ae8b2d624d06c837c2e52e4022c
STEPANYANA <a.stepanyan@elsevier.com>
2016-05-10 13:30:24 +0200

    Boilerplate implemented

--- src/app.js
+++ src/app.js
@@ -1,5 +1,46 @@
 import React, { Component } from '../node_modules/react';
 import {render} from 'react-dom';
-import Login from './login/login';
+import KanbanBoard from './kanbanboard/kanbanboard';
 
-render(<Login  />, document.getElementById('container'));
+let cardList = [
+  {
+    "id": 1,
+    "title": "Card one title",
+    "description": "Card detailed description.",
+    "status": "todo",
+    "tasks": [
+      {
+        "id": 1,
+        "name": "Task one",
+        "done": true
+      },
+      {
+        "id": 2,
+        "name": "Task two",
+        "done": false
+      },
+      {
+        "id": 3,
+        "name": "Task three",
+        "done": false
+      }
+    ]
+  },
+  {
+    "id": 2,
+    "title": "Card Two title",
+    "description": "Card detailed description",
+    "status": "in-progress",
+    "tasks": []
+  },
+  {
+    "id": 3,
+    "title": "Card Three title",
+    "description": "Card detailed description",
+    "status": "done",
+    "tasks": []
+  }
+];
+
+
+render(<KanbanBoard cards={cardList} />, document.getElementById('container'));
---
+++ src/list/list.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Card from '../card/card';
+
+class List extends Component {
+  render() {
+    let cards = this.props.cards.map(function (card) {
+      return <Card
+        id={card.id}
+        key={card.id}
+        title={card.title}
+        description={card.description}
+        tasks={card.tasks} />
+    });
+    return (
+      <div className="list">
+        <h1>
+          {this.props.title}
+        </h1>
+        {cards}
+      </div>
+    );
+  }
+}
+export default List;
