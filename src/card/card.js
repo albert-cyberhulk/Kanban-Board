a0695295371a0c0940b816b2ae74b107202d81e4
Albert Cyberhulk <albert@cyberhulk.net>
2016-05-20 15:33:00 +0200

    Adding state toggle

--- src/card/card.js
+++ src/card/card.js
@@ -6,30 +6,21 @@ class Card extends Component {
     super(...arguments);
     this.state = {
       showDetails: false
-    }
+    };
   }
   render () {
     let cardDetails;
     var isHidden = this.state.showDetails ? "card__details slide-down" : "card__details slide-up";
     return (
       <div className="card">
-        <i className="fa fa-folder card_title" aria-hidden="true" onClick={()=>this.setState({showDetails: !this.state.showDetails})}>{this.props.title}</i>
+        <i className="fa fa-folder card_title" aria-hidden="true" onClick={()=>this.setState({showDetails: !this.state.showDetails})}>
+          {this.props.title}
+        </i>
         <div className={isHidden}>
           {this.props.description}
           <Checklist cardId={this.props.id} tasks={this.props.tasks} />
         </div>
       </div>
-    );
-  }
-}
-
-class Test extends Component {
-  constructor() {
-    super(...arguments);
-  }
-  render() {
-    return (
-      <h1>Hel</h1>
     );
   }
 }
