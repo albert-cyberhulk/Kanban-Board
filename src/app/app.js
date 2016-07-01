fc45c2a524cbab03406098b743c4311ed8b1136a
Albert Cyberhulk <albert@cyberhulk.net>
2016-07-01 15:39:57 +0200

    Removing unused comments

--- src/app/app.js
+++ src/app/app.js
@@ -4,8 +4,6 @@
  * @author Albert Cyberhulk
  */
 
-// Initial json file
-// http://www.jsoneditoronline.org/?id=01e50ab7586fd69363abd6866bce63f3
 
 import React, { Component } from '../../node_modules/react';
 import {render} from 'react-dom';
